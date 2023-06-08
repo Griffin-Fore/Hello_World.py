6126a887ffdad36ad3c59e25f0a7f0b37aa19631
Griffin Fore <griffinfore@gmail.com>
2023-06-08 14:48:43 -0700

    Second trime

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,17 @@
 # 1. TASK: print "Hello World"
 print("Hello World")
 # 2. print "Hello Noelle!" with the name in a variable
-name = "Noelle"
+name = "Griffin"
 print("Hello", name,"!")	# with a comma
 print("Hello " + name + "!")	# with a +
 # 3. print "Hello 42!" with the number in a variable
-name = 42
+name = 7
 print("Hello", name)	# with a comma
-# print("Hello" + name)	# with a +	-- this one should give us an error!
+name_to_str = str(name)
+print("Hello " + name_to_str)	# with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
-fave_food2 = "pizza"
+fave_food2 = "steak"
 print("I love to eat {} and {}.".format(fave_food1,fave_food2)) # with .format()
 print(f"I love to eat {fave_food1} and {fave_food2}.") # with an f string
 
